66360d12de9bb8b194dbde93e5223f66bede71c7
ruben <r.hamers@bluetea.nl>
2018-09-10 07:28:57 +0200

    commit before leiningen project generation

--- src/ProjectSetup/ProjectSetup.clj
+++ src/ProjectSetup/ProjectSetup.clj
@@ -3,11 +3,10 @@
 (defn sum [ x y z]
   (+ x y z))
 
-(defmacro unless [condition & body]
+(defmacro unless "Evaluates the body only when the condition is false." [condition & body]
   `(if (not ~condition)
      (do ~@body)))
 
 (def flatList `(1 2 3 ~@(list 4 5 6)))
-(def value2 flatList)
 
 (def value 10)
---
+++ src/ThreeBuyerProtocol/ThreeBuyerProtocol.clj
@@ -0,0 +1,1 @@
+(ns ThreeBuyerProtocol.ThreeBuyerProtocol)
