cb873d6edfee8ae9ca1fb9025bbacd160bf54737
ruben <r.hamers@bluetea.nl>
2018-10-03 07:28:20 +0200

    working on date generator function

--- src/discourje/TwoBuyerProtocol.clj
+++ src/discourje/TwoBuyerProtocol.clj
@@ -13,11 +13,26 @@
 
 ;Define a method to send a message with a tag on a channel
 (defn send-with-tag [msg tag channel]
-      (>!! channel {:tag tag
-                 :msg (:msg msg)}))
+      (>!! channel {:msg msg :tag tag}))
 
 
-(send-with-tag "test" :quote buyer1)
+(let [cal (java.util.Calendar/getInstance)
+      d (new java.util.Date)]
+  (doto cal
+    (.setTime d)
+    (.add java.util.Calendar/DATE 90)
+    (.getTime)))
+
+(defn getDate [days]
+  (let [c (java.util.Calendar/getInstance)
+        d (new java.util.Date)]
+    (.add (.setTime (java.util.Calendar/getInstance) (new java.util.Date)) days)
+    (.getTime)))
+(getDate 2)
+
+(send-with-tag 30 :quote buyer1)
+(send-with-tag "no integer" :quote buyer1)
+(send-with-tag "not received" :unusedTag buyer1)
 
 ;first subscribe to :title tag on sellers channel, and when received, publish quote to the buyers
 (let [s seller]
@@ -27,26 +42,29 @@
   (sub (publish s) :quit s)     ;subscribe to publish on channel s:seller
   (when-let [message (<! s)]
     (cond
-      (= message :title)
+      (= (:tag message ) :title)
       (let [quote (+ (rand-int 30) 1)]                          ;use let construct to define a quote variable as a random number between 1 and 30
         (go (send-with-tag quote :quote buyer1))
         (go (send-with-tag quote :quote buyer2)))
-      (= message :ok) 1
-      (= message :address) (go (send-with-tag "03-10-18" :date buyer2))
-      (= message :quit) 3)))        ;send a message (random number between 1 and 30) with tag `quote' over to buyer 1 and 2
+      (= (:tag message ) :ok) (println "Ok confirmation received!")
+      (= (:tag message ) :address) (go (send-with-tag "03-10-18" :date buyer2))
+      (= (:tag message ) :quit) 3)))        ;send a message (random number between 1 and 30) with tag `quote' over to buyer 1 and 2
 
 ;subscribe buyer 1 to :quote tag
 (let [b1 buyer1]
   (sub (publish b1) :quote b1)                      ;subscribe to channel
-  (go (send-with-tag (rand-int (<! b1)) :quoteDiv buyer2))) ;fire quote div (random number in quote value)
+  (go (let [content (<! b1)]
+        (if (number? (:msg content))
+          ((print "Yes, message content is a number!")
+           (send-with-tag (rand-int (:msg content)) :quoteDiv buyer2))))))
 
-;subscribe buyer 1 to  :quote tag
+;subscribe buyer 2 to  :quote tag
 (let [b2 buyer2]
   (sub (publish b2) :quote b2)
   (sub (publish b2) :quoteDiv b2) ;subscribe to channel on tag quote and quoteDiv
   (sub (publish b2) :date b2) ;subscribe to channel on tag quote and quoteDiv
   (when-let [message (<! b2)]
     (cond
-      (= message :quote) 1
-      (= message :quoteDiv) 2
-      (= message :date) 3))) ;fire quote div (random number in quote value)
+      (= (:tag message ) :quote) 1
+      (= (:tag message ) :quoteDiv) 2
+      (= (:tag message ) :date) 3))) ;fire quote div (random number in quote value)
