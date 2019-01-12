45e5b8f6dacae717d818080dcfbeb4859cba6342
Ruben Hamers <ruben_hamers@hotmail.com>
2019-01-12 10:31:56 +0100

    working on api, wrinting functions first, then converting them to macros

--- src/discourje/api/api.clj
+++ src/discourje/api/api.clj
@@ -1,7 +1,8 @@
 (ns discourje.api.api
   (require [discourje.core.monitor :refer :all]
-                              [discourje.core.protocol :refer :all]
-                              [discourje.core.dataStructures :refer :all]))
+           [discourje.core.protocol :refer :all]
+           [discourje.core.protocolCore :refer :all]
+           [discourje.core.dataStructures :refer :all]))
 
 (defn monitor-send
   "make send monitor, sending action from sender to receiver."
@@ -12,3 +13,18 @@
   "make receive monitor, listening for action on receiver send by sender."
   [action sender receiver]
   (->receiveM action receiver sender))
+
+(defn generateProtocolFromMonitors
+  "generate the protocol, channels and set the first monitor active"
+  [monitors]
+  (generateProtocol monitors))
+
+(defn send!
+  "send action with value from sender to receiver"
+  [action value sender receiver]
+  (send-to sender action value receiver))
+
+(defn recv!
+  "receive action from sender on receiver, invoking callback"
+  [action sender receiver callback]
+  (receive-by receiver action sender callback))
