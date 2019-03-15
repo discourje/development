900bca35c70589ef67ef1416de039d116244454b
Ruben Hamers <ruben_hamers@hotmail.com>
2019-03-15 07:00:23 +0100

    added first draft of macros

---
+++ src/discourje/core/async/macros.clj
@@ -0,0 +1,21 @@
+;macros.clj
+(in-ns 'discourje.core.async.async)
+
+;[rec] create recursion macro
+(defmacro rec
+  "Generate recursion"
+  [name interaction & more]
+  `(make-recursion name (vector interaction more)))
+;[continue] create continue recursion macro
+(defmacro continue
+  "Continue recursion, matched by name"
+  [name]
+  `(do-recur name))
+;[choice] create choice macro
+(defmacro choice
+  "Generate choice"
+  [branch & more]
+  `(make-choice (vector branch more)))
+;[mep] specify mep macro
+(defmacro mep [interactions]
+  `(create-protocol interactions))
