0c277405aba90648daf00923f7e86a33186fd52f
Ruben Hamers <ruben_hamers@hotmail.com>
2019-05-30 08:56:49 +0200

    fixed the linking process for atomic interactions only to test scatter gather asap

--- src/discourje/core/referencedInteractionLinking.clj
+++ src/discourje/core/referencedInteractionLinking.clj
@@ -2,24 +2,25 @@
 (in-ns 'discourje.core.async)
 
 (def interz [(-->> 1 "a" "b")
-             (-->> 1 "b" "c")
-             (-->> 1 "c" "d")])
+             (-->> 2 "b" "c")
+             (-->> 3 "c" "d")])
 
-(defn link-interactions-by-reference [interactions]
-  (let [linked-interactions (atom [])]
-    (doseq [i interactions]
-      (let [inter (atom i)]
-        (println @inter)
-        (cond
-          (empty? @linked-interactions)
-          (swap! linked-interactions conj inter)
-          (satisfies? interactable i)
-          (do (swap! (last @linked-interactions) assoc (:next (last @linked-interactions)) inter)
-              (swap! linked-interactions conj inter)
-              )
-          :else
-          (log-error :unsupported-reference-link "unsupported reference link interaction")
-          )))
-    @linked-interactions))
+(defn- assoc-next-atom "assoc the inter to the :next key of the previous" [prev inter]
+  (assoc prev :next inter))
 
-(println (link-interactions-by-reference interz))
+(defn link-interactions=by-reference [interactions]
+  (let [first-interaction (atom (first interactions))]
+    (if (< 1 (count interactions))
+      (loop [i 1
+             new-inter first-interaction]
+        (let [inter (atom (nth interactions i))]
+          (cond
+            (satisfies? interactable (nth interactions i))
+            (do (swap! new-inter assoc-next-atom inter)
+                (when (true? (< i (- (count interactions) 1)))
+                  (recur (+ 1 i) inter)))
+            :else
+            (log-error :unsupported-reference-link "unsupported reference link interaction"))))
+      first-interaction)))
+
+(println (link-interactions=by-reference interz))
