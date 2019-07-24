0440d4b71c991bd82c6ee6f70f1f01ae5ee520c9
Ruben Hamers <ruben_hamers@hotmail.com>
2019-07-24 06:43:22 +0200

    added recursion to referenced interaction linking, we are unable to print or inspect the data structure now since it has recursion, which blows the stack when trying to print it for example. the next step is to remove all atom references to solve the infinite recursion loop and only allow for it once.

--- src/discourje/core/referencedInteractionLinking.clj
+++ src/discourje/core/referencedInteractionLinking.clj
@@ -9,10 +9,15 @@
   [coll x]
   (conj (pop coll) x))
 
+(defn- remove-atoms [root-interaction]
+    (loop [unreferenced-root @root-interaction]
+     continue here with removing all atom references to single ref )
+  )
+
 (defn link-interactions-by-reference
   ([protocol]
    (link-interactions-by-reference (get-interactions protocol) 1)
-   )
+    )
   ([index interactions]
    (let [first-interaction (atom (first interactions))]
      (when (< 1 (count interactions))
@@ -21,24 +26,32 @@
          (let [inter (atom (nth interactions i))]
            (cond
              (satisfies? branchable @new-inter)
-             (doseq [b (get-branches @new-inter)] (loop [current-branch-inter b]
-                                                    (if (nil? (get-next @current-branch-inter))
-                                                      (swap! current-branch-inter assoc-next-atom inter)
-                                                      (recur (get-next @current-branch-inter)))))
-             (satisfies? interactable (nth interactions i))
-             (do (swap! new-inter assoc-next-atom inter)
-                 ;(when (true? (< i (- (count interactions) 1)))
-                 ;  (recur (+ 1 i) inter))
-                 )
+             (doseq [b (get-branches @new-inter)]
+               (loop [current-branch-inter b]
+                 (if (nil? (get-next @current-branch-inter))
+                   (swap! current-branch-inter assoc-next-atom inter)
+                   (recur (get-next @current-branch-inter)))))
+             ;(satisfies? recursable @new-inter)
+             ;(loop [next-interaction (get-recursion @new-inter)]
+             ;  (if (and (satisfies? identifiable-recur @next-interaction) (= (get-name @next-interaction) (get-name @new-inter)))
+             ;    (swap! next-interaction assoc-next-atom new-inter)
+             ;    (recur (get-next @next-interaction))))
+             (or (satisfies? interactable (nth interactions i)) (satisfies? identifiable-recur (nth interactions i)))
+             (swap! new-inter assoc-next-atom inter)
              (satisfies? branchable (nth interactions i))
-             (do (swap! new-inter assoc-next-atom inter)
-                 (let [linked-branches (for [b (get-branches (nth interactions i))](link-interactions-by-reference 1 b))]
-                   (swap! inter assoc :branches linked-branches)
-                   ))
+             (let [linked-branches (for [b (get-branches (nth interactions i))] (link-interactions-by-reference 1 b))]
+               (swap! new-inter assoc-next-atom inter)
+               (swap! inter assoc :branches linked-branches)
+               )
+             (satisfies? recursable (nth interactions i))
+             (let [linked-recursion (link-interactions-by-reference 1 (get-recursion (nth interactions i)))]
+               (swap! new-inter assoc-next-atom inter)
+               (swap! inter assoc :recursion linked-recursion))
              :else
              (log-error :unsupported-reference-link "unsupported reference link interaction"))
            (when (true? (< i (- (count interactions) 1)))
              (recur (+ 1 i) inter)))))
+     (remove-atoms first-interaction)
      first-interaction)))
 
 (def interz [(-->> 1 "a" "b")
@@ -47,6 +60,12 @@
              (choice [(-->> 4 "1" "2") (-->> 5 "1" "2")]
                      [(-->> 6 "1" "2") (-->> 7 "1" "2")])
              (-->> 8 "v" "b")
-             (-->> 9 "v" "b")])
+             (-->> 9 "v" "b")
+             (rec :test
+                  (-->> 11 "1" "2")
+                  (-->> 12 "1" "3")
+                  (continue :test)
+                  )
+             (-->> "sdasd" "1" "2")])
 ;
 (println (link-interactions-by-reference 1 interz))
