33be6d74b2d813999a39f2faf684fdf61309851f
Sung-Shik <ssj@ou.nl>
2019-08-10 16:19:28 +0200

    Added some preliminary experimental stuff (see src/discourje/examples/experimental/): process-algebraic syntax for the dsl (including looping construct; see .../dsl.clj), a thin convenience wrapper around the api (see .../api.clj), and examples (tictactoe and ring; see .../ttt/ and .../ring/) with basic time-bounded (instead of iteration-bounded) benchmark support

---
+++ src/discourje/examples/experimental/ring/discourje.clj
@@ -0,0 +1,29 @@
+(ns discourje.examples.experimental.ring.discourje
+  (require [discourje.examples.experimental.dsl :refer :all]
+           [discourje.examples.experimental.api :refer :all]))
+
+(defn alicefn
+  [i in out k]
+  (if (= i 0)
+    ; alice 0
+    (doseq [_ (range k)] (>!! out i) (<!! in))
+    ; alice 1 <= i <= k
+    (doseq [_ (range k)] (<!! in) (>!! out i))))
+
+(defn ring-discourje
+  ([time k m]
+   (ring-discourje time k 1000 m))
+  ([time k n m]
+   (let [chans (forv [i (range k)] (chan 1 (alice i) (alice (+ i 1)) m))]
+     (bench time #(let [aaa (forv [i (range k)]
+                                  (thread (alicefn i
+                                                   (get chans (mod (- i 1) k))
+                                                   (get chans i)
+                                                   n)))]
+                    (join aaa) (monitor-reset m))))))
+
+(ring-discourje 1 2
+                (monitor (spec {:k 2}
+                               (fix :X (seq (rep seq [:i (range :k)]
+                                                 (--> (alice :i) (alice (inc :i)) Long))
+                                            (fix :X))))))
