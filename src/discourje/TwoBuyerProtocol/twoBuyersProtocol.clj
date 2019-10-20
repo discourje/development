4083fb714e2dcc1934881779debd6d9dadc96039
Ruben Hamers <ruben_hamers@hotmail.com>
2019-10-20 16:36:09 +0200

    removed commented code

--- src/discourje/TwoBuyerProtocol/twoBuyersProtocol.clj
+++ src/discourje/TwoBuyerProtocol/twoBuyersProtocol.clj
@@ -6,16 +6,6 @@
            [discourje.TwoBuyerProtocol.Seller :as s])
   (:import (clojure.lang PersistentArrayMap)
            (java.util GregorianCalendar)))
-;(def two-buyer-protocol
-;  (mep
-;    (-->> String "buyer1" "seller")
-;    (-->> Long "seller" ["buyer1" "buyer2"])
-;    (-->> Long "buyer1" "buyer2")
-;    (choice
-;      [(-->> PersistentArrayMap "buyer2" "seller")
-;       (-->> String "buyer2" "seller")
-;       (-->> Long "seller" "buyer2")]
-;      [(-->> PersistentArrayMap "buyer2" "seller")])))
 ;define two buyer protocol, Notice: it is extended with recursion!
 (def two-buyer-protocol
   (mep
