91cbcf8a7acaa5188b6afc5652efbe86e3beaf17
Sung-Shik Jongmans <ssj@ou.nl>
2020-03-24 08:00:21 +0100

    Fixed existing tests (parameterizedRecTests.clj partially merged into syntax_tests.clj)

--- src2/test/clojure/discourje/core/async/impl/dsl/syntax_tests.clj
+++ src2/test/clojure/discourje/core/async/impl/dsl/syntax_tests.clj
@@ -3,6 +3,10 @@
             [discourje.core.async.impl.test-data :refer :all]
             [discourje.core.async.impl.dsl.syntax :refer :all]))
 
+;;;;
+;;;; monitorTests.clj
+;;;;
+
 (deftest get-active-interaction-test
   (let [mon (generate-spec (testDualProtocol false))]
     (println mon)
@@ -109,6 +113,56 @@
   (let [mon (generate-spec (rec-with-parallel-with-choice-multicast-and-close false))]
     (is (not-empty @(:recursion-set mon)))
     (is (= (get-active-interaction mon) rec-with-parallel-with-choice-multicast-and-closeControl))))
+
+;;;;
+;;;; parameterizedRecTests.clj (partial)
+;;;;
+
+(defn single-recur-protocol-params [include-ids]
+  (if include-ids (create-protocol [(make-interaction (message-checker "1") "A" "B")
+                                    (make-recursion [:test {:r1 "A" :r2 "B" :r3 "C"}]
+                                                    [(make-interaction (message-checker "1") :r2 :r1)
+                                                     (make-choice [
+                                                                   [(make-interaction (message-checker "2") :r1 :r3)
+                                                                    (make-interaction (message-checker "2") :r3 :r1)
+                                                                    (do-recur [:test [:r1 :r2 :r3]])]
+                                                                   [(make-interaction (message-checker "3") :r1 :r2)
+                                                                    ]
+                                                                   ])
+                                                     ])
+                                    (make-interaction (message-checker "end") "A" ["B" "C"])
+                                    ])
+                  (create-protocol [(->interaction nil nil "A" "B" #{} nil)
+                                    (->recursion nil [:test {:r1 "A" :r2 "B" :r3 "C"}]
+                                                 [(->interaction nil nil :r2 :r1 #{} nil)
+                                                  (->branch nil [
+                                                                 [(->interaction nil nil :r1 :r3 #{} nil)
+                                                                  (->interaction nil nil :r3 :r1 #{} nil)
+                                                                  (->recur-identifier nil [:test [:r1 :r2 :r3]] :recur nil)]
+                                                                 [(->interaction nil nil :r1 :r2 #{} nil)]] nil)
+                                                  ] nil)
+                                    (->interaction nil nil "A" ["B" "C"] #{} nil)
+                                    ])))
+
+(def single-recur-protocol-paramsControl
+  (->interaction nil nil "A" "B" #{}
+                 (->interaction nil nil "B" "A" #{}
+                                (->branch nil [
+                                               (->interaction nil nil "A" "C" #{}
+                                                              (->interaction nil nil "C" "A" #{}
+                                                                             (->recur-identifier nil [:test [:r1 :r2 :r3]] :recur nil)))
+                                               (->interaction nil nil "A" "B" #{} (->interaction nil nil "A" ["B" "C"] #{} nil))
+
+                                               ] nil))
+                 )
+  )
+
+(deftest single-recur-protocol-params-test
+  (let [mon (generate-spec (single-recur-protocol-params false))]
+    (is (= (get-active-interaction mon) single-recur-protocol-paramsControl))))
+
+(deftest unique6-roles-single-recur-params-test
+  (is (= 6 (count (get-distinct-role-pairs (get-interactions (single-recur-protocol-params true)))))))
 
 ;;;;
 ;;;; rolesTests.clj
