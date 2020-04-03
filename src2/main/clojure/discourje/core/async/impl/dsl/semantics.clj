73b1e67495351148fa01950662341046ed5f0ff7
Sung-Shik Jongmans <ssj@ou.nl>
2020-04-03 12:25:02 +0200

    State space generation

---
+++ src2/main/clojure/discourje/core/async/impl/dsl/semantics.clj
@@ -0,0 +1,262 @@
+(ns discourje.core.async.impl.dsl.semantics
+  (:require [discourje.core.async.impl.dsl.syntax :as syntax]
+            [clojure.walk :as w])
+  (:import (java.util.function Function Predicate)
+           (discourje.core.async.impl.lts Send Receive Close)))
+
+(defn visualize [lts mcrl2-root-dir tmp-file]
+  (spit tmp-file (.toString lts))
+  (clojure.core.async/thread
+    (clojure.java.shell/sh (str mcrl2-root-dir "/bin/ltsgraph") tmp-file)))
+
+(defn- smash [spec]
+  (if (and (vector? spec) (= (count spec) 1))
+    (smash (first spec))
+    spec))
+
+(defn- smap [spec]
+  (zipmap (:vars spec) (map eval (:exprs spec))))
+
+(defn substitute [spec smap]
+  ;(println spec)
+  (cond
+
+    ;; End
+    (= (:type spec) :end)
+    spec
+
+    ;; Action
+    (contains? syntax/action-types (:type spec))
+    (w/postwalk-replace smap spec)
+
+    ;; Choice
+    (= (:type spec) :choice)
+    (syntax/choice (mapv #(substitute % smap) (:branches spec)))
+
+    ;; Parallel
+    (= (:type spec) :parallel)
+    (syntax/parallel (mapv #(substitute % smap) (:branches spec)))
+
+    ;; If
+    (= (:type spec) :if)
+    (syntax/if-then-else (w/postwalk-replace smap (:condition spec))
+                         (substitute (:branch1 spec) smap)
+                         (substitute (:branch2 spec) smap))
+
+    ;; Loop
+    (= (:type spec) :loop)
+    (syntax/loop (:name spec)
+                 (:vars spec)
+                 (w/postwalk-replace smap (:exprs spec))
+                 (substitute (:body spec) (apply dissoc smap (:vars spec))))
+
+    ;; Recur
+    (= (:type spec) :recur)
+    (syntax/recur (:name spec)
+                  (w/postwalk-replace smap (:exprs spec)))
+
+    ;; Sequence
+    (vector? spec)
+    (mapv #(substitute % smap) spec)
+
+    ;; Application
+    (coll? spec)
+    (concat [(first spec)]
+            (w/postwalk-replace smap (rest spec)))
+
+    :else (throw (Exception.))))
+
+(defn unfold [loop spec]
+  ;(println "unfold: " (:type spec))
+  (cond
+
+    ;; End
+    (= (:type spec) :end)
+    spec
+
+    ;; Action
+    (contains? syntax/action-types (:type spec))
+    spec
+
+    ;; Choice
+    (= (:type spec) :choice)
+    (syntax/choice (mapv #(unfold loop %) (:branches spec)))
+
+    ;; Parallel
+    (= (:type spec) :parallel)
+    (syntax/parallel (mapv #(unfold loop %) (:branches spec)))
+
+    ;; If
+    (= (:type spec) :if)
+    (syntax/if-then-else (:condition spec)
+                         (unfold loop (:branch1 spec))
+                         (unfold loop (:branch2 spec)))
+
+    ;; Loop
+    (= (:type spec) :loop)
+    (syntax/loop (:name spec)
+                 (:vars spec)
+                 (:exprs spec)
+                 (if (= (:name loop) (:name spec)) (:body spec) (unfold loop (:body spec))))
+
+    ;; Recur
+    (= (:type spec) :recur)
+    (if (= (:name loop) (:name spec))
+      (syntax/loop (:name loop) (:vars loop) (:exprs spec) (:body loop))
+      spec)
+
+    ;; Sequence
+    (vector? spec)
+    (mapv #(unfold loop %) spec)
+
+    ;; Application
+    (coll? spec)
+    spec
+
+    :else (throw (Exception.))))
+
+(defn terminated? [spec]
+  (cond
+
+    ;; End
+    (= (:type spec) :end)
+    true
+
+    ;; Action
+    (contains? syntax/action-types (:type spec))
+    false
+
+    ;; Choice
+    (= (:type spec) :choice)
+    (not (not-any? terminated? (:branches spec)))
+
+    ;; Parallel
+    (= (:type spec) :parallel)
+    (every? terminated? (:branches spec))
+
+    ;; If
+    (= (:type spec) :if)
+    (terminated? (if (eval (:condition spec)) (:branch1 spec) (:branch2 spec)))
+
+    ;; Loop
+    (= (:type spec) :loop)
+    (terminated? (unfold spec (substitute (:body spec) (smap spec))))
+
+    ;; Recur
+    (= (:type spec) :recur)
+    (throw (Exception.))
+
+    ;; Sequence
+    (vector? spec)
+    (every? terminated? spec)
+
+    ;; Application
+    (coll? spec)
+    (let [name (first spec)
+          vals (map eval (rest spec))
+          body (:body (get (get @syntax/registry name) (count vals)))
+          vars (:vars (get (get @syntax/registry name) (count vals)))]
+      (terminated? (substitute body (zipmap vars vals))))
+
+    :else (throw (Exception.))))
+
+(defn successors [spec]
+  ;(println "successors: " (:type spec))
+  (cond
+
+    ;; End
+    (= (:type spec) :end)
+    {}
+
+    ;; Action
+    (contains? syntax/action-types (:type spec))
+    (let [role-fn (fn [role]
+                    (if (coll? role)
+                      (apply ((if (fn? (first role)) identity eval) (first role)) (map eval (rest role)))
+                      (eval role)))
+          sender (role-fn (:sender (:channel spec)))
+          receiver (role-fn (:receiver (:channel spec)))
+          predicate (:predicate spec)]
+      (cond (= (:type spec) :send)
+            {(reify
+               Send
+               (getSender [_] (.toString sender))
+               (getReceiver [_] (.toString receiver))
+               (getPredicate [_] (reify Predicate (test [this message] true)))
+               Object
+               (toString [_] (str "!(" sender "," receiver "," predicate ")")))
+             (syntax/end)}
+            (= (:type spec) :receive)
+            {(reify
+               Receive
+               (getSender [_] (.toString sender))
+               (getReceiver [_] (.toString receiver))
+               (getPredicate [_] (reify Predicate (test [this message] true)))
+               Object
+               (toString [_] (str "?(" sender "," receiver "," predicate ")")))
+             (syntax/end)}
+            (= (:type spec) :close)
+            {(reify
+               Close
+               (getSender [_] (.toString sender))
+               (getReceiver [_] (.toString receiver))
+               Object
+               (toString [_] (str "C(" sender "," receiver ")")))
+             (syntax/end)}))
+
+    ;; Choice
+    (= (:type spec) :choice)
+    (reduce merge (map successors (:branches spec)))
+
+    ;; Parallel
+    (= (:type spec) :parallel)
+    (let [branches (:branches spec)]
+      (loop [i 0
+             result {}]
+        (if (= i (count branches))
+          result
+          (let [branch (nth branches i)
+                m (successors branch)
+                f #(syntax/parallel (into (subvec branches 0 i) (into [%] (subvec branches (inc i) (count branches)))))]
+            (recur (inc i) (merge result (if (empty? m) {} (update-in m (keys m) f))))))))
+
+    ;; If
+    (= (:type spec) :if)
+    (successors (if (eval (:condition spec)) (:branch1 spec) (:branch2 spec)))
+
+    ;; Loop
+    (= (:type spec) :loop)
+    (successors (unfold spec (substitute (:body spec) (smap spec))))
+
+    ;; Recur
+    (= (:type spec) :recur)
+    (throw (Exception.))
+
+    ;; Sequence
+    (vector? spec)
+    (if (empty? spec)
+      {}
+      (if (terminated? (first spec))
+        (successors (vec (rest spec)))
+        (let [m (successors (first spec))]
+          (update-in m (keys m)
+                     #(smash (into (if (terminated? %) [] [%]) (rest spec)))))))
+
+    ;; Application
+    (coll? spec)
+    (let [name (first spec)
+          vals (mapv eval (rest spec))
+          body (:body (get (get @syntax/registry name) (count vals)))
+          vars (:vars (get (get @syntax/registry name) (count vals)))]
+      (successors (substitute body (zipmap vars vals))))
+
+    :else (throw (Exception.))))
+
+(def expander
+  (reify
+    Function
+    (apply [_ spec]
+      ; (println (str "to expand: " spec))
+      (let [m (successors spec)]
+        ; (println (str "expanded: " m))
+        m))))
