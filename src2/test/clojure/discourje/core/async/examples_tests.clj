e22626480509274e780988b1ee00ab893e24b8d6
Sung-Shik Jongmans <ssj@ou.nl>
2020-05-31 22:42:02 +0200

    Minor

--- src2/test/clojure/discourje/core/async/examples_tests.clj
+++ src2/test/clojure/discourje/core/async/examples_tests.clj
@@ -14,54 +14,54 @@
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.ring]
-                    {:buffered [true] :k [2] :secs [1]}))
+                    {:buffered [true] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.ring]
-                    {:buffered [false] :k [2] :secs [1]}))
+                    {:buffered [false] :k [2] :secs [0.1]}))
   (is true)
 
   ;; Mesh
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.mesh]
-                    {:buffered [true] :k [2] :secs [1]}))
+                    {:buffered [true] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.mesh]
-                    {:buffered [false] :k [2] :secs [1]}))
+                    {:buffered [false] :k [2] :secs [0.1]}))
   (is true)
 
   ;; Star
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [true] :ordered-sends [true] :ordered-receives [true] :k [2] :secs [1]}))
+                    {:buffered [true] :ordered-sends [true] :ordered-receives [true] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [true] :ordered-sends [true] :ordered-receives [false] :k [2] :secs [1]}))
+                    {:buffered [true] :ordered-sends [true] :ordered-receives [false] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [true] :ordered-sends [false] :ordered-receives [true] :k [2] :secs [1]}))
+                    {:buffered [true] :ordered-sends [false] :ordered-receives [true] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [true] :ordered-sends [false] :ordered-receives [false] :k [2] :secs [1]}))
+                    {:buffered [true] :ordered-sends [false] :ordered-receives [false] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [false] :ordered-sends [true] :k [2] :secs [1]}))
+                    {:buffered [false] :ordered-sends [true] :k [2] :secs [0.1]}))
   (is true)
 
   (print (e/run-all [:clj :dcj]
                     ['discourje.core.async.examples.micro.star]
-                    {:buffered [false] :ordered-sends [false] :k [2] :secs [1]}))
+                    {:buffered [false] :ordered-sends [false] :k [2] :secs [0.1]}))
   (is true))
