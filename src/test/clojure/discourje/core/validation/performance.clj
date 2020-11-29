6983523ffd99c7acc741f73b7ffa30f69a4a05bb
Erik Horlings <erik.horlings@gino.nl>
2020-11-29 11:12:37 +0100

    Improved test harness for performance tests.

--- src/test/clojure/discourje/core/validation/performance.clj
+++ src/test/clojure/discourje/core/validation/performance.clj
@@ -1,16 +1,28 @@
 (ns discourje.core.validation.performance
-  (:require [discourje.core.spec :as s]))
+  (:require [discourje.core.spec :as s]
+            [discourje.core.spec.lts :as lts]))
 
 (s/defrole ::a)
 (s/defrole ::b)
 (s/defrole ::c)
+(s/defrole ::d)
 
-(s/defsession ::huge-lts []
-              (let [ids (set (range 4))]
-                (s/par-every [i ids]
-                             [(s/--> String (::a i) (::b i))
-                              (s/--> String (::b i) (::c i))
-                              (s/close (::a i) (::b i))
-                              (s/close (::b i) (::c i))
-                              ]
-                             )))
+(s/defsession ::huge-lts [size]
+              (s/par (s/cat-every [i (range size)]
+                                  [
+                                   (s/--> String (::a i) (::a (+ i 1)))
+                                   (s/close (::a i) (::a (+ i 1)))
+                                   ])
+                     (s/cat-every [i (range size)]
+                                  [(s/--> String (::b i) (::b (+ i 1)))
+                                   (s/close (::b i) (::b (+ i 1)))
+                                   ])
+                     (s/cat-every [i (range size)]
+                                  [(s/--> String (::c i) (::c (+ i 1)))
+                                   (s/close (::c i) (::c (+ i 1)))
+                                   ])
+                     ))
+
+(defn get-huge-lts
+  [size]
+  (lts/lts (s/session ::huge-lts [size])))
--- src/test/java/discourje/core/validation/ModelCheckerPerformanceTest.java
+++ src/test/java/discourje/core/validation/ModelCheckerPerformanceTest.java
@@ -1,29 +1,67 @@
 package discourje.core.validation;
 
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
 import discourje.core.lts.LTS;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.Test;
 
 class ModelCheckerPerformanceTest<Spec> extends AbstractModelCheckerTest<Spec> {
 
     @Test
-    public void testPerformanceOnLargeLts() {
-        System.out.println("Timings in seconds:");
+    public void testIncreasingLtsSize() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        printHeader();
+        long i = 1;
+        outer:
+        while (true) {
+            for (int j = 0; j < 3; j++) {
+                long finalI = i;
+                long t0 = System.currentTimeMillis();
+                Future<?> future = executor.submit(() -> testPerformanceOnLargeLts(finalI));
+                try {
+                    future.get(10, TimeUnit.MINUTES);
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException e) {
+                    break outer;
+                }
+                if (System.currentTimeMillis() - t0 > (2 * 60_000)) {
+                    break outer;
+                }
+            }
+            i++;
+        }
+    }
 
+    public void testPerformanceOnLargeLts(long size) {
         long t0 = System.currentTimeMillis();
-        LTS<Spec> lts = getLTS("huge-lts");
+        LTS<Spec> lts = getHugeLTS(size);
         lts.expandRecursively();
         long t1 = System.currentTimeMillis();
         int ltsSize = lts.getStates().size();
-        System.out.println("LTS size (#states):      " + ltsSize );
-        System.out.println("LTS creation:            " + (t1 - t0)/1000.0 );
 
         ModelChecker modelChecker = new ModelChecker(lts);
         long t2 = System.currentTimeMillis();
-        System.out.println("Abstract model creation: " + (t2 - t1)/1000.0 );
 
         modelChecker.checkModel();
         long t3 = System.currentTimeMillis();
-        System.out.println("Labelling:               " + (t3 - t2)/1000.0 );
+        System.out.printf("%s;%s;%s;%s;%s%n", size, ltsSize, (t1 - t0)/1000.0, (t2 - t1)/1000.0, (t3 - t2)/1000.0 );
     }
 
+    private void printHeader() {
+        System.out.println("run #;#states;LTS creation;Model creation;Labelling");
+    }
+
+    LTS<Spec> getHugeLTS(long size) {
+        IFn var = Clojure.var("discourje.core.validation.performance", "get-huge-lts");
+        @SuppressWarnings("unchecked")
+        LTS<Spec> lts = (LTS<Spec>) var.invoke(size);
+        return lts;
+    }
 }
