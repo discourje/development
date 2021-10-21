b867009914c6cf177876aaf292e515e8b0e700d0
Sung-Shik Jongmans <ssj@ou.nl>
2021-10-21 13:14:40 +0200

    Added timeout to main

--- src/main/clojure/discourje/examples/main.clj
+++ src/main/clojure/discourje/examples/main.clj
@@ -14,10 +14,14 @@
             l/*exclude* (if (some? (:exclude settings)) (:exclude settings) l/*exclude*)
             mcrl2/*mcrl2-bin* (:mcrl2-bin settings)
             mcrl2/*mcrl2-tmp* (:mcrl2-tmp settings)]
+
+    (when (some? (:timeout settings))
+      (.start (Thread. ^Runnable (fn []
+                                   (Thread/sleep (* 1000 (:timeout settings)))
+                                   (prn "timeout")
+                                   (System/exit 0)))))
     (let [begin (System/nanoTime)
-          _ (try
-              (require program :reload)
-              (catch Throwable t (.printStackTrace t)))
+          _ (require program :reload)
           end (System/nanoTime)]
       {:settings settings
        :program  program
@@ -33,18 +37,19 @@
     (let [[settings program-short input] (read-string (str "[" (clojure.string/join " " args) "]"))
           program (symbol (str "discourje.examples" "." program-short))
           output (main settings program input)]
-      (prn output))
+      (prn output)
+      (System/exit 0))
 
     (catch Throwable t
       (println)
       (println (str "Discourje Examples (" (commit) ")"))
       (println (str "Usage: java -jar discourje-examples.jar <settings> <program> <input>"))
-      (println)
       (println (str "  <program> \u2208 {"
                     (clojure.string/join ", " ["micro.mesh" "micro.ring" "micro.star"
                                                "games.chess" "games.go-fish" "games.rock-paper-scissors" "games.tic-tac-toe"
                                                "npb3.cg" "npb3.ft" "npb3.is" "npb3.mg"])
                     "}"))
+      (println)
       (.printStackTrace t)
       (println))))
 
